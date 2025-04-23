d29e210197f464dcb72f34d68721da40385a5aa8
AlPakh <alvp2003@yandex.com>
2025-04-23 22:39:57 +0300

    first commit

---
+++ src/test/java/com/example/Bex/BexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
